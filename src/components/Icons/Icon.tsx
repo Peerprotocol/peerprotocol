cb9c5b16a847442605eea7f459166fefa18446c2
Vantyse <nonsokingsley61@gmail.com>
2024-03-19 12:12:44 +0100

    fix: build issues

--- src/components/Icons/Icon.tsx
+++ src/components/Icons/Icon.tsx
@@ -1,7 +1,7 @@
 import { IconPropTypes } from "../../lib/types";
 import Icons from "./Icons";
 
-export default function ({
+export default function Icon({
   name,
   ...props
 }: IconPropTypes & { name: keyof typeof Icons }) {
