6614f320383457f853e4648794070123aa549ec9
imdavyking <yungdynamic53@gmail.com>
2024-03-23 16:54:42 +0100

    fixing contract

---
+++ contracts/target/debug/deps/contracts-678da2b408eda080.d
@@ -0,0 +1,6 @@
+/Users/davyking/Work/peerprotocol/contracts/target/debug/deps/libcontracts-678da2b408eda080.rmeta: lib.rs states.rs
+
+/Users/davyking/Work/peerprotocol/contracts/target/debug/deps/contracts-678da2b408eda080.d: lib.rs states.rs
+
+lib.rs:
+states.rs:
