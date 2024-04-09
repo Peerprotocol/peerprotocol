584d2ee0a873b4db7e23b9142a4d435e7d884c35
imdavyking <yungdynamic53@gmail.com>
2024-04-09 06:36:26 +0100

    fixed

--- src/components/borrowComponent.tsx
+++ src/components/borrowComponent.tsx
@@ -7,8 +7,21 @@ import Pagination from "./Pagination";
 import usePagination from "@/lib/hooks/usePagination";
 import { infoDataType } from "@/lib/types";
 import { PAGE_SIZE } from "@/lib/constants";
+import { useUserState } from "@/hooks/user_states";
 
 const BorrowComponent = () => {
+  const {
+    initializeUser,
+    transactionPending,
+    initialized,
+    loading,
+    deposit,
+    lent,
+    depositCollaterial,
+    loans,
+  } = useUserState();
+
+  console.log(loans);
   const {
     paginatedItems: paginatedTableData,
     totalPages,
@@ -18,7 +31,7 @@ const BorrowComponent = () => {
   return (
     <div className="w-full">
       <div className="bg-[#FFFFFF0D] p-4 rounded-xl mb-3 pb-6">
-        <InfoTable tableItems={paginatedTableData} />
+        <InfoTable tableItems={loans} />
       </div>
       <div className="flex justify-end">
         <Pagination
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,11 +1,11 @@
 import { SVGProps } from "react";
 
 export interface infoDataType {
-  borrower: string;
-  assets: string;
-  netvalue: string;
-  interestRate: string;
-  duration: string;
+  // borrower: string;
+  // assets: string;
+  // netvalue: string;
+  // interestRate: string;
+  // duration: string;
 }
 
 export interface IconPropTypes extends SVGProps<SVGSVGElement> {}
