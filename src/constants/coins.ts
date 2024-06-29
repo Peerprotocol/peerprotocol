0f574e8a4e3b7ee9bbcad52a212e4b6193f98ce1
Akachukwu Chukwuemerie David <63329343+Imdavyking@users.noreply.github.com>
2024-06-29 02:20:15 +0100

    fixed

--- src/constants/coins.ts
+++ src/constants/coins.ts
@@ -5,6 +5,8 @@ interface Coin {
   mint_address: string;
   pool_address: string;
   image: string;
+  from_ata: string;
+  from_ata_pda: string;
 }
 
 export const allowedCoins: Coin[] = [
@@ -13,11 +15,15 @@ export const allowedCoins: Coin[] = [
     mint_address: "B2Mc28Ehe4m4TvqJhAVVGnucuKwLgPg88qB9zX87WQEd",
     pool_address: "",
     image: "/image/logo.png",
+    from_ata: "",
+    from_ata_pda: "",
   },
   {
     ticker: "USDC",
     mint_address: "4zMMC9srt5Ri5X14GAgXhaHii3GnPAEERYPJgZJDncDU",
     pool_address: "5SSkXsEKQepHHAewytPVwdej4epN1nxgLVM84L4KXgy7",
     image: "/image/usdc.png",
+    from_ata: "cqYNVxjS7Xin1LmfM7KMwqKockNZpa4yiPkJ1L8ZvWN",
+    from_ata_pda: "9BzsJTjC7N2y1qCYAhtYFy1FdNxAUYyfbTiz8XevTVBE",
   },
 ];
