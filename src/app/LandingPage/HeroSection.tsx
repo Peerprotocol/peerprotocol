1d8da2d9bda55ce0735b02bbfa7625add3e55cd9
Deon <oluwadamilare_daniel@outlook.com>
2024-07-21 03:58:34 +0100

    fix

--- src/app/LandingPage/HeroSection.tsx
+++ src/app/LandingPage/HeroSection.tsx
@@ -71,7 +71,7 @@ const HeroPage = () => {
         <div
           className={`${
             !isDarkMode ? "bg-[#0d101711]" : "bg-current"
-          } py-30 lg:m-0 xl:m-0 mx-auto bg-[#F5F5F5] text-text xl:mt-[1rem] mt-[-10rem] xl:w-full w-full px-32 md:mt-0 sm:m-0 sm:pb-10`}
+          } py-30 lg:m-0 xl:m-0 mx-auto bg-[#fff] text-text xl:mt-[1rem] mt-[-10rem] xl:w-full w-full px-32 md:mt-0 sm:m-0 sm:pb-10`}
         >
           <p className={`${isDarkMode ? "text-white" : "text-gray-600"} font-semibold text-white py-2 lg:text-base text-sm text-center pt-8`}>
             Interoperable with
