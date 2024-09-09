333f5c9fdc37675a36c90f0d3693060b457eff03
DanielEmmanuel1 <oluwadamilare_daniel@outlook.com>
2024-09-09 22:43:29 +0100

    Feat: UserProfile update

--- src/app/peerprotocolapp/userProfile/Dashboard.tsx
+++ src/app/peerprotocolapp/userProfile/Dashboard.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import Image from 'next/image';
 import Info from "../../../../public/images/info.svg";
+import { dashboardItems } from './DashboardData'; // Import the data
 
 const Dashboard = () => {
     return (
         <div className="border border-gray-200 rounded-[1rem] grid grid-cols-3 gap-6 p-6 bg-white flex-grow">
-            {[...Array(5)].map((_, index) => (
+            {dashboardItems.map((item, index) => (
                 <div key={index} className='border border-gray-200 p-6 rounded-xl bg-smoke-white relative flex flex-col justify-center'>
-                    <p className='text-sm text-gray-400'>Net Value</p>
-                    <p className='text-[2rem] text-black'>$1900.01</p>
-                    <Image
-                        src={Info}
-                        height={20}
-                        width={20}
-                        alt="info-icon"
-                        className="cursor-pointer absolute top-2 right-3"
-                    />
+                    <p className='text-sm text-gray-400'>{item.label}</p>
+                    <p className='text-[2.5rem] font-medium text-gray-700'>{item.value}</p>
+                    {item.info && (
+                        <Image
+                            src={Info}
+                            height={20}
+                            width={20}
+                            alt="info-icon"
+                            className="cursor-pointer absolute top-2 right-3"
+                        />
+                    )}
                 </div>
             ))}
         </div>
